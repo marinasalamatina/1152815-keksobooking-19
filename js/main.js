d505f22d6a220e2248f35bdfd816dd11bf1fb572
marinasalamatina <marinasalamatina@mail.ru>
2020-02-20 15:57:03 +0300

    Add settings active/noactive form

--- js/main.js
+++ js/main.js
@@ -20,17 +20,39 @@ var OFFER_MIN_GUESTS = 1;
 var OFFER_MAX_GUESTS = 4;
 var OFFER_DESCRIPTIONS = ['описание1', 'описание2', 'описание3', 'описание4', 'описание5'];
 
-var markMap = document.querySelector('#pin').content.querySelector('.map__pin');
-var markImage = document.querySelector('#pin').content.querySelector('img');
-var mapCard = document.querySelector('#card').content.querySelector('.map__card');
+var ENTER_KEYCODE = 13;
+var MAX_PRICENIGHT = 1000000;
+
 var map = document.querySelector('.map');
+
+var mapPin = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
+var mapPinMain = document.querySelector('.map__pin--main');
+var pinImage = document.querySelector('#pin').content.querySelector('img');
+var mainPinImage = mapPinMain.querySelector('img');
+
+var adForm = document.querySelector('.ad-form');
+var adFormAddress = adForm.querySelector('#address');
+var adFormRooms = adForm.querySelector('#room_number');
+var adFormType = adForm.querySelector('#type');
+var adFormPrice = adForm.querySelector('#price');
+var adFormCapacity = adForm.querySelector('#capacity');
+var adFormFieldsets = adForm.querySelectorAll('fieldset');
+
+var mapCard = document.querySelector('#card').content.querySelector('.map__card');
 
 var offerTypeList = {
-  'flat': 'Квартира',
   'bungalo': 'Бунгало',
+  'flat': 'Квартира',
   'house': 'Дом',
   'palace': 'Дворец'
+};
+
+var minPriceForTypeList = {
+  'bungalo': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000
 };
 
 var getRandomNumber = function (min, max) {
@@ -94,40 +116,39 @@ var getAdsContent = function () {
   return adsContent;
 };
 
-var createMark = function (adContent) {
-  var mark = markMap.cloneNode(true);
+var createPin = function (adContent) {
+  var pin = mapPin.cloneNode(true);
 
   var rect = document.querySelector('.map__pin').querySelector('img').getBoundingClientRect();
   var imageWidth = rect.width;
   var imageHeight = rect.height;
 
-  var markLocationLeft = adContent.location.x - imageWidth / 2 + 'px';
-  var markLocationTop = adContent.location.y - imageHeight + 'px';
+  var pinLocationLeft = adContent.location.x - imageWidth / 2 + 'px';
+  var pinLocationTop = adContent.location.y - imageHeight + 'px';
 
-  mark.style.left = markLocationLeft;
-  mark.style.top = markLocationTop;
-  markImage.src = adContent.author.avatar;
-  markImage.alt = adContent.offer.title;
+  pin.style.left = pinLocationLeft;
+  pin.style.top = pinLocationTop;
+  pinImage.src = adContent.author.avatar;
+  pinImage.alt = adContent.offer.title;
 
-  return mark;
+  return pin;
 };
 
-var createMarks = function (adsContent) {
-  var marks = document.createDocumentFragment();
+var createPins = function (adsContent) {
+  var pins = document.createDocumentFragment();
   adsContent = getAdsContent();
 
   for (var i = 0; i < adsContent.length; i += 1) {
-    marks.appendChild(createMark(adsContent[i]));
+    pins.appendChild(createPin(adsContent[i]));
   }
-  return marks;
+  return pins;
 };
-
 
 var getFeatures = function (features) {
   var featuresListFragment = document.createElement('ul');
   featuresListFragment.classList.add('popup__features');
 
-  for (var i = 0; i < features.length; i++) {
+  for (var i = 0; i < features.length; i += 1) {
     var featuresItemFragment = document.createElement('li');
     featuresItemFragment.classList.add('popup__feature');
     featuresItemFragment.classList.add('popup__feature--' + features[i]);
@@ -139,7 +160,7 @@ var getFeatures = function (features) {
 var getPhotos = function (template, photos) {
   var photosFragment = document.createDocumentFragment();
 
-  for (var i = 0; i < photos.length; i++) {
+  for (var i = 0; i < photos.length; i += 1) {
     var photoFragment = template.cloneNode(true);
     photoFragment.src = photos[i];
     photosFragment.appendChild(photoFragment);
@@ -187,9 +208,102 @@ var createCards = function () {
 var setCards = function () {
   var cards = createCards();
 
-  map.classList.remove('map--faded');
   mapPins.appendChild(cards);
-  mapPins.appendChild(createMarks());
+  mapPins.appendChild(createPins());
 };
 
-setCards();
+var noActiveMap = function () {
+  map.classList.add('map--faded');
+  adForm.classList.add('ad-form--disabled');
+
+  getAdFormAddress();
+
+  mapPinMain.addEventListener('mousedown', mapPinMainMousedown);
+  mapPinMain.addEventListener('keydown', mainPinEnterKeydown);
+  mapPinMain.addEventListener('mousedown', getAdFormAddress);
+  mapPinMain.addEventListener('keydown', getAdFormAddress);
+
+  disableElements(adFormFieldsets);
+};
+
+var disableElements = function (array) {
+  for (var i = 0; i < array.length; i += 1) {
+    array[i].setAttribute('disabled', '');
+  }
+};
+
+var noDisableElements = function (array) {
+  for (var i = 0; i < array.length; i += 1) {
+    array[i].removeAttribute('disabled');
+  }
+};
+
+
+var mapPinMainMousedown = (function (evt) {
+  evt.preventDefault();
+  if (evt.button === 0) {
+    activeMap();
+  }
+});
+
+var mainPinEnterKeydown = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    activeMap();
+  }
+};
+
+var activeMap = function () {
+  setCards();
+
+  map.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+
+  adFormAddress.setAttribute('readonly', '');
+  adFormCapacity.addEventListener('change', adFormRoomsChange);
+  adFormRooms.addEventListener('change', adFormRoomsChange);
+  adFormType.addEventListener('change', adFormTypeChange);
+  adFormPrice.addEventListener('change', adFormTypeChange);
+
+  getAdFormAddress();
+
+  mapPinMain.removeEventListener('keydown', mainPinEnterKeydown);
+  mapPinMain.removeEventListener('mousedown', mapPinMainMousedown);
+  mapPinMain.removeEventListener('mousedown', getAdFormAddress);
+  mapPinMain.removeEventListener('keydown', getAdFormAddress);
+
+  noDisableElements(adFormFieldsets);
+};
+
+var adFormRoomsChange = function () {
+  var rooms = adFormRooms.value;
+  var guests = adFormCapacity.value;
+
+  var validityMessage = (guests > rooms) ? 'Нужно выбрать больше комнат или уменьшить число гостей' : '';
+
+  adFormCapacity.setCustomValidity(validityMessage);
+};
+
+var getAdFormAddress = function () {
+  var rect = mainPinImage.getBoundingClientRect();
+  var mapPinMainLeft = rect.left;
+  var mapPinMainTop = rect.top;
+  var mapPinMainWidth = rect.width;
+  var mapPinMainHeight = rect.height;
+
+  var mapPinLocationX = Math.round(mapPinMainLeft + mapPinMainWidth / 2);
+  var mapPinLocationY = Math.round(mapPinMainTop + mapPinMainHeight);
+  var mapPinCoordinates = mapPinLocationX + ', ' + mapPinLocationY;
+
+  adFormAddress.value = mapPinCoordinates;
+};
+
+var adFormTypeChange = function () {
+  var minPrice = minPriceForTypeList[adFormType.value];
+  adFormPrice.placeholder = minPrice;
+
+  var validityMessage = (adFormPrice.value < minPrice) ? 'Рекомендуемая цена за ночь от ' + minPrice + ' до ' + MAX_PRICENIGHT : '';
+
+  adFormPrice.setCustomValidity(validityMessage);
+};
+
+noActiveMap();
