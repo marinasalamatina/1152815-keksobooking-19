99ba4b0a367fbc15f6021ed717c2721d69403043
marinasalamatina <marinasalamatina@mail.ru>
2020-03-19 13:51:48 +0300

    Finish fix filter logic

--- js/filter.js
+++ js/filter.js
@@ -7,67 +7,64 @@
   var roomsFilter = filtersContainer.querySelector('#housing-rooms');
   var priceFilter = filtersContainer.querySelector('#housing-price');
   var typeFilter = filtersContainer.querySelector('#housing-type');
-  var featuresContainer = filtersContainer.querySelector('.map__features');
-  var features = featuresContainer.querySelectorAll('.map__checkbox');
+  var mapFilters = filtersContainer.querySelector('.map__filters');
 
   var filterType = function (card) {
-    return ((typeFilter.value === card.offer.type) || (typeFilter.value === 'any')) ? false : true;
+    return ((typeFilter.value === card.offer.type) || (typeFilter.value === 'any')) ? true : false;
   };
 
   var filterPrice = function (card) {
     switch (priceFilter.value) {
       case 'any':
-        return false;
-      case 'low':
-        return (card.offer.price < window.constants.filterPrice.LOW_PRICE) ? false : true;
-      case 'high':
-        return (card.offer.price >= window.constants.filterPrice.HIGH_PRICE) ? false : true;
-      case 'middle':
-        return ((card.offer.price >= window.constants.filterPrice.LOW_PRICE) && (card.offer.price <= window.constants.filterPrice.HIGH_PRICE)) ? false : true;
-      default:
         return true;
+      case 'low':
+        return (card.offer.price < window.constants.filterPrice.LOW_PRICE) ? true : false;
+      case 'high':
+        return (card.offer.price >= window.constants.filterPrice.HIGH_PRICE) ? true : false;
+      case 'middle':
+        return ((card.offer.price >= window.constants.filterPrice.LOW_PRICE) && (card.offer.price <= window.constants.filterPrice.HIGH_PRICE)) ? true : false;
+      default:
+        return false;
     }
-  };
-
-  var filterGuests = function (card) {
-    return ((guestsFilter.value === card.offer.guests) || (guestsFilter.value === 'any')) ? false : true;
   };
 
   var filterRooms = function (card) {
-    return ((roomsFilter.value === card.offer.rooms) || (guestsFilter.value === 'any')) ? false : true;
+    return ((roomsFilter.value === card.offer.rooms.toString()) || (roomsFilter.value === 'any')) ? true : false;
+  };
+
+  var filterGuests = function (card) {
+    return ((guestsFilter.value === card.offer.guests.toString()) || (guestsFilter.value === 'any')) ? true : false;
   };
 
   var filterFeature = function (card) {
+    var checkboxesChecked = mapFilters.querySelectorAll('.map__checkbox:checked');
     var featuresChecked = [];
-    var featuresFilter = document.querySelector('#housing-features');
-    var featuresInputs = Array.from(featuresFilter.querySelectorAll('input'));
-    for (var i = 0; i < featuresInputs.length; i += 1) {
-      if (featuresInputs[i].checked) {
-        featuresChecked.push(featuresInputs[i].value);
-      }
-    }
-    featuresChecked.every(function (elem) {
-      var cardFeatures = Array.from(card.offer.features);
-      for (var i = 1; i < featuresChecked.length; i += 1) {
-        if (cardFeatures[i] === elem) {
 
-        }
-      }
-       }
-      });
+    checkboxesChecked.forEach(function (checkbox) {
+      featuresChecked.push(checkbox.value);
     });
+
+    if (featuresChecked.length > 0) {
+      return featuresChecked.every(function (feature) {
+        return card['offer']['features'].includes(feature);
+      });
+    }
+    return true;
   };
 
   var validateCard = function (card) {
-    return ((!filterType(card)) && (!filterPrice(card)) && (!filterRooms(card)) && (!filterGuests(card)) && (!filterFeature(card))) ? false : true;
+    return ((filterType(card)) && (filterPrice(card)) && (filterRooms(card)) && (filterGuests(card)) && (filterFeature(card))) ? true : false;
   };
 
   var checkFilters = function (cards) {
     var filteredCards = [];
     for (var i = 0; i < cards.length; i += 1) {
       var isCardCorrect = validateCard(cards[i]);
-      if (!isCardCorrect) {
+      if (isCardCorrect) {
         filteredCards.push(cards[i]);
+        if (filteredCards.length > window.constants.pinParameters.ADS_NUMBER) {
+          return filteredCards;
+        }
       }
     }
     return filteredCards;
